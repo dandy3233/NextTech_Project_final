93c5c5f09e8d58c5ba42ee734121b750678bbe56
dandy tak <dandytakilu@gmail.com>
2026-09-25 16:42:05 +0300

    fix vercel react routerrr

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The backend server address
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://192.168.1.16:8000';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 
 
